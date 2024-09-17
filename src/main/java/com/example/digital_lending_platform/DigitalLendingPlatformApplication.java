b336eb661c5884ea890f1e2a5b501762ef4258e3
Laurettta <lauretta.okolie@mtn.com>
2024-09-17 16:39:44 +0100

    first commit

---
+++ src/main/java/com/example/digital_lending_platform/DigitalLendingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.digital_lending_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalLendingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalLendingPlatformApplication.class, args);
+	}
+
+}
